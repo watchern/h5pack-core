e631d9acd27c20c7d949dbeae90a796b538a0526
watchern <watchern@126.com>
2025-05-31 03:23:33 +0800

    github action 更换docker-dist

--- example/docker/dist/assets/use-id-MmIoJa9H.js
+++
@@ -1,1 +0,0 @@
-import{aj as e}from"./index-D1fUOvX-.js";let r=0;function u(){const n=e(),{name:t="unknown"}=(n==null?void 0:n.type)||{};return"".concat(t,"-").concat(++r)}export{u};
---
+++ example/docker/dist/assets/use-id-PAlPqZ9M.js
@@ -0,0 +1,1 @@
+import{aj as e}from"./index--8V8yV1D.js";let r=0;function u(){const n=e(),{name:t="unknown"}=(n==null?void 0:n.type)||{};return"".concat(t,"-").concat(++r)}export{u};
